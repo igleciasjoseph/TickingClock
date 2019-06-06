091054c5bc443c18552ddb3b489ea600ddbd4ecb
Joseph Iglecias <igleciasjoseph@gmail.com>
2019-06-06 14:52:24 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { get } from 'https';
 
 function App() {
+  window.onload = function () {
+    var myDate = setInterval(clockTime, 1000);
+    function clockTime() {
+      var d = new Date();
+      document.getElementById('dt').innerHTML = d.toLocaleTimeString();
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <p>The current time is: <span id="dt"></span></p>
     </div>
   );
 }
-
 export default App;
